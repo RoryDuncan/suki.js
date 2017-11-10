6f0bec3e09a403d36aaf2feb176bb0c68ed5d44a
Rory Duncan <robust.rory@gmail.com>
2017-11-10 10:32:27 +0000

    Refactor Context

--- context.js
+++ context.js
@@ -1,69 +1,59 @@
 
-const nonChainableContextItems = [
+const valueReturningMethods = [
   "getImageData",
   "createImageData",
   "isPointInStroke",
   "isPointInPath"
-];
+]
 
-
-// not defined as a class because we programmatically create the prototype below.
-function Context() {
-  
-  this.canvas = document.createElement("canvas")
-  this.context = this.canvas.getContext("2d")
-  
+let chainMethod = function(wrapper, fn, isGetter) {
+  if (isGetter) {
+    return (...args) => fn.apply(wrapper.context, ...args)
+  }
+  else return (...args) => {
+    fn.apply(wrapper.context, ...args) 
+    return wrapper
+  }
 }
 
-Context.prototype = (function () {
-  
-  let proto = {}
-  
-  let canvas = document.createElement("canvas")
-  let context = canvas.getContext("2d")
-  
-  // used for chaining context methods
-  let chainMethod = function(fn, isGetter) {
-    if (isGetter) {
-      return (...args) => fn.apply(this.context, ...args)
+// chains a property as a function
+// ->   ctx.text(value) instead of ctx.text = value
+let chainProperty = function(wrapper, key) {
+  return (value) => {
+    if (typeof value == "undefined") {
+      return wrapper.context[key]
     }
-    else return (...args) => {
-      fn.apply(this.context, ...args) 
-      return this;
-    }
+    wrapper.context[key] = value
+    return wrapper
   }
-  
-  // chains a property as a function
-  // ->   ctx.text(value) instead of ctx.text = value
-  let chainProperty = function(key) {
-    return (value) => {
-      if (typeof value == "undefined") {
-        return this.context[key]
-      }
-      this.context[key] = value;
-      return this;
-    }
-  }
-  
-  // for-in iterates over prototype methods, as well
-  for (let key in context) {
-    let value = context[value]
-    let isGetter = false
-    
-    // detect if it's a method or a property
-    if (typeof value == "function") {
-      
-      if (nonChainableContextItems.includes(key)) {
-        isGetter = true
-      }
-      
-      proto[key] = chainMethod(value, isGetter)
-    }
-    else if (key != "canvas") {
-      proto[key] = chainProperty(key)
-    }
-  }
-  return proto;
-})()
+}
 
-export default Context
+// not defined as a class because we programmatically create the prototype below.
+export default class Context {
+  
+  constructor() {
+    
+    this.canvas = document.createElement("canvas")
+    this.context = this.canvas.getContext("2d")
+  
+    // extend the canvas's context
+    // for-in iterates over prototype methods, as well
+    for (let key in context) {
+      let value = context[value]
+      let isGetter = false
+      
+      // detect if it's a method or a property
+      if (typeof value == "function") {
+        
+        if (valueReturningMethods.includes(key)) {
+          isGetter = true
+        }
+        
+        this[key] = chainMethod(this, value, isGetter)
+      }
+      else if (key != "canvas") {
+        this[key] = chainProperty(this, key)
+      }
+    }
+  }
+}
