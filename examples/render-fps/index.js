abf2b5b7009eca87a0682404e551d07775280e14
Rory Duncan <robust.rory@gmail.com>
2017-11-12 06:37:26 +0000

    Update render-fps example

--- examples/render-fps/index.js
+++ examples/render-fps/index.js
@@ -16,6 +16,7 @@ suki.events.on("step", (time) => {
 suki.events.on("render", (time, $) => {
   $.clear("#07c")
     .fillStyle("#fff")
-    .fillText(`${time.fps.toFixed(0)} fps`, 50, 50)
-    .fillText(`${suki.stats.skippedFrames} frames skipped`, 50, 75)
+    .fillText(`${time.ticks} ticks`, 50, 20)
+    .fillText(`${suki.stats.fps().toFixed(0)} fps`, 50, 35)
+    .fillText(`${suki.stats.skippedFrames} frames skipped`, 50, 50)
 })
