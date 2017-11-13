cb3834cd2c79a1840d87211fd1c8b5d87c71ec0b
Rory Duncan <robust.rory@gmail.com>
2017-11-13 09:42:37 +0000

    Update dev

--- dev/index.js
+++ dev/index.js
@@ -3,20 +3,34 @@ import Suki from "../index"
 let suki = new Suki()
 
 console.log(suki)
+
+  /*
+    In the class below we're extendedin suki.App().
+    
+    This returns a prototype with .mount(), and .unmount() methods, 
+    which add listeners for the following lifecycle events of your suki app:
+    
+    tick, step, preRender, render, and postRender
+  
+  */
+class Thing extends suki.App() {
+  
+  
+  render(time, renderer, ref) {
+    let text = "Hello from Thing Class. This is being called from an instance of Thing.render."
+    renderer
+      .clear()
+      .fillStyle("#000")
+      .fillText(text, 200, 200)
+  }
+}
+
 suki.ready(() => {
+  let thing = new Thing()
+  thing.mount()
+  console.log(thing)
   console.log("ready!")
   suki.start()
 })
 
-suki.events.on("step", (time) => {
-  
-})
 
-
-suki.events.on("render", (time, $) => {
-  $.clear("#07c")
-    .fillStyle("#fff")
-    .fillText(`${time.ticks} ticks`, 50, 20)
-    .fillText(`${suki.stats.fps().toFixed(0)} fps`, 50, 35)
-    .fillText(`${suki.stats.skippedFrames} frames skipped`, 50, 50)
-})
