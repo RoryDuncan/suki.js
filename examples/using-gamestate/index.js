a9451f7d83850a60d9a75a2be1e792fa74ac18d6
Rory Duncan <robust.rory@gmail.com>
2017-12-10 11:23:39 +0000

    Update example to switch between two states

--- examples/using-gamestate/index.js
+++ examples/using-gamestate/index.js
@@ -1,37 +1,73 @@
 import Suki from "../../index"
 import { gamestate, State } from "../../state"
 
+//
+// In this example:
+// we create two states and change between them using clicks
+// we also measure how long it takes to change states.. 
+//    though it's not of real value and we don't expect it to be non-performant
+//
+
+
 let suki = new Suki()
 
 // set a reference to suki for all of our gamestates to utilize
+// required!
 gamestate.ref = suki
+
+
+var stateChangeDuration = 0.00
 
 // create a title screen state
 // when inside the title screen state, we render the menu items
-const title = new State({
+const titleState = new State({
   
   name: "title-screen",
   
   render: (time, $, suki) => {
     $.clear("#07c")
       .fillStyle("#fff")
-      .font("20px Arial")
+      .font("15px Arial")
       
       
-      ;[
-        "Suki.js",
-        "> Play",
-        "> Options",
-        "> Credits",
-        "> Exit"
-      ].forEach((a, i) => $.fillText(a, 100, 100 + (i * 25)))
-      
-    
+      $.fillText("Blue State", 100, 150)
+      $.fillText("Click to change to toggle current state", 100, 175)
+      $.fillText(`Changing to this state took ${stateChangeDuration}ms`, 100, 190)
   }
 })
 
-gamestate.add(title)
-gamestate.change(title.name)
+const playState = new State({
+  
+  name: "play-game",
+  
+  render: (time, $, suki) => {
+    $.clear("#707")
+      .fillStyle("#fff")
+      .font("15px Arial")
+      
+      $.fillText("Maroon State", 100, 150)
+      $.fillText("Click to change to toggle current state", 100, 175)
+      $.fillText(`Changing to this state took ${stateChangeDuration}ms`, 100, 190)
+      
+  }
+})
+
+
+gamestate.add(titleState)
+gamestate.add(playState)
+
+gamestate.change(titleState.name)
+
+// change between the states when clicking
+var toggle = false;
+document.addEventListener("click", (e) => {
+  let nextState = toggle ?  titleState : playState ;
+  let start = performance.now() /* global performance */
+  gamestate.change(nextState.name)
+  stateChangeDuration = (performance.now() - start).toFixed(2)
+  toggle = !toggle
+})
+
 
 console.log(suki)
 suki.whenReady(() => {
